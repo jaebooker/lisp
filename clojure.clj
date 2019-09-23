9cedab9c181c532ebcf973284ac4ddf565a73f8c
jaebooker <jaesonbooker@gmail.com>
2019-09-23 13:50:13 -0700

    first blood

---
+++ clojure.clj
@@ -0,0 +1,12 @@
+(defn atlas-v
+ []
+  {:dry-mass  50050
+   :fuel-mass 284450
+   :time 0
+   :isp 3050
+   :max-fuel-rate (/ 284450 253)
+   :max-thrust 4.152e6})
+ (defn atlas-v
+  []
+   {:dry-mass 50050
+    :fuel-mass 284450})
