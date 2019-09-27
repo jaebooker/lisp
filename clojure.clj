d8d6930b7250493135723a72e9519b947e0d9f98
jaebooker <jaesonbooker@gmail.com>
2019-09-27 14:46:31 -0700

    a lisp

--- clojure.clj
+++ clojure.clj
@@ -1,12 +1,8 @@
 (defn atlas-v
- []
-  {:dry-mass  50050
-   :fuel-mass 284450
-   :time 0
-   :isp 3050
-   :max-fuel-rate (/ 284450 253)
-   :max-thrust 4.152e6})
- (defn atlas-v
   []
    {:dry-mass 50050
-    :fuel-mass 284450})
+    :fuel-mass 284450
+    :time 0
+    :isp 3050
+    :max-fuel-rate (/ 28445 253)
+    :max-thrust 4.152e6})
